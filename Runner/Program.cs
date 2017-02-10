b1386e6e259383f80794bb35aa0a0b7a8d13fd85
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-02-10 16:35:08 +0100

    Setup as an helper receiving the container

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -33,52 +33,43 @@ namespace Runner
             using (var container = builder.Build())
             {
                 Console.WriteLine("## Requesting a ScheletonSetup");
+                
+                
+                var businessLogic2 = container.Resolve<Func<BusinessLogic2>>();
+                RegisterConsumerDomainLogic(container, new SetupQueue2(), businessLogic2);
 
-                //                var scheletonSetup = container.Resolve<ScheletonSetup>();
-                //
-                //                scheletonSetup.Setup(new SetupQueue1());
-                //
-                //                var consumerFactory = container.Resolve<Func<IModel, MyConsumer>>();
-                //                scheletonSetup.RegisterConsumer(consumerFactory, 5, Queue1);
-
-                var scheletonSetup2 = container.Resolve<ScheletonSetup<Payload2>>();
-                var businessLogic2 = container.Resolve<BusinessLogic2>();
-                scheletonSetup2.RegisterConsumerDomainLogic(new SetupQueue2(), businessLogic2);
-
-
-//                var scheletonSetup3 = container.Resolve<ScheletonSetup<Payload3>>();
-//                var businessLogic3 = container.Resolve<BusinessLogic3>();
-//                scheletonSetup3.RegisterConsumerDomainLogic(new SetupQueue3(), businessLogic3);
-
-
-                var myExtendedConsumer = container.Resolve<MyExtendedConsumer>();
-                myExtendedConsumer.Register();
-
+    
                 Console.WriteLine("## Waiting.... Press Enter to stop");
                 Console.ReadLine();
             }
             Console.WriteLine("## Disposed. Press Enter to shutdown");
             Console.ReadLine();
         }
+
+
+        public string Setup(IContainer container, IRabbitSetup rabbitSetup)
+        {
+            Console.WriteLine("## Setting up environment");
+            var channel = container.Resolve<IModel>();
+
+            return rabbitSetup.Execute(channel);
+        }
+
+        public void RegisterConsumerDomainLogic<T>(IContainer container, IRabbitSetup rabbitSetup, Func<IConsumerBusinessLogic<T>> businessLogic)
+        {
+            var channel = container.Resolve<IModel>();
+            var queue = Setup(container, rabbitSetup);
+
+            Console.WriteLine($"## Creating a default consumer with the given domain logic and registering it");
+
+
+            var defaultConsumerFactory = container.Resolve<Func<IModel, IConsumerBusinessLogic<T>, DefaultConsumer<T>>>();
+
+            var defaultConsumer = defaultConsumerFactory(channel, businessLogic());
+            
+            channel.BasicConsume(queue: queue, noAck: true, consumer: defaultConsumer);
+        }
+
+
     }
-
-    class MyExtendedConsumer : DefaultBaseConsumer<Payload3>
-    {
-        public MyExtendedConsumer(IModel channel) : base(channel)
-        {
-        }
-
-        protected override void Handle(Payload3 payload)
-        {
-            Console.WriteLine("ricevuto");
-        }
-
-        protected override string Setup(IModel channel)
-        {
-            channel.QueueDeclare(Program.Queue3, durable: true, exclusive: true, autoDelete: false);
-            channel.BasicConsume(Program.Queue3, noAck: true, consumer: this);
-            return Program.Queue3;
-        }
-    }
-
 }
